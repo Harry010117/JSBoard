b7ec97414470fe61709a0b32464e8fe33d184c88
Harry <harry010117@gmail.com>
2018-06-14 17:21:15 +0900

    asdf

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(session({
-	secret: 'SeCrEt',
+	secret: 'mySecret',
 	resave: false,
 	saveUninitialized: true
 }));
